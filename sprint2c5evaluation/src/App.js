4113d7a8578719a5a1771131590e91b3da66a25f
uday2925 <udaykiran1139@gmail.com>
2022-04-18 16:57:59 +0530

    completed

--- sprint2c5evaluation/src/App.js
+++ sprint2c5evaluation/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [show,setShow]=useState(true)
   return (
     <div className="App">
-      <button className="togglebtn" onClick={()=>{setShow(!show)}}>Add a new student</button>
+      <button className="togglebtn" onClick={()=>{setShow(!show)}}>{show?"Add a new student":"ShowStudents"}</button>
       {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
       {show?<ShowStudents/>:<AddStudent/>}
       {/* make sure the table is shown initially, do not show form initially */}
